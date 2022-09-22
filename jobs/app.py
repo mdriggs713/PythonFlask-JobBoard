92f12f3af846258194e0744174faf3f310e374aa
mdriggs713 <113919825+mdriggs713@users.noreply.github.com>
2022-09-22 16:04:36 -0600

    layout21

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,5 @@
 import sqlite3
+from unittest import result
 from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
@@ -12,6 +13,22 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+@app.teardown_appcontext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+    
 
 
 
